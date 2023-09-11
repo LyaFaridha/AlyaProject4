d56e0d4702a871c0da514f48c0563e89a9c39fbf
LyaFaridha <143927970+LyaFaridha@users.noreply.github.com>
2023-09-12 02:38:08 +0800

    Update AlyaProject.py

--- AlyaProject.py
+++ AlyaProject.py
@@ -8,7 +8,7 @@ server = app.server
 app.title = "MCM7003 Data Visualization Interactive Demo"
 
 # Load data from the provided URL
-data_url = "https://raw.githubusercontent.com/LyaFaridha/Assignment3/main/df_arabica_clean.csv"
+data_url = "https://raw.githubusercontent.com/LyaFaridha/AlyaProject4/main/df_arabica_clean.csv"
 df = pd.read_csv(data_url)
 
 # Create a bar chart for the 'Country of Origin' counts
@@ -36,11 +36,47 @@ fig2 = px.box(df, x='Processing Method', y=['Aroma', 'Flavor'],
              title='Comparison of Aroma and Flavor Attributes across Processing Methods')
 
 app.layout = html.Div(
-    [html.H1("Data Visualization"),
-     dcc.Graph(id='graph-output', figure=fig_country_counts),
-	dcc.Graph(id='graph1-output', figure=fig1),
-        dcc.Graph(id='graph2-output', figure=fig2)]
+    [
+        html.H1("Data Visualization"),
+        dcc.Checklist(
+            id='show-chart',
+            options=[
+                {'label': 'Show Country Counts Chart', 'value': 'show-country-counts'}
+            ],
+            value=[],  # Initialize with no selected options
+        ),
+        dcc.RadioItems(
+            id='select-graph',
+            options=[
+                {'label': 'Country Counts', 'value': 'country-counts'},
+                {'label': 'Relationship between Aroma and Flavor', 'value': 'aroma-flavor'},
+                {'label': 'Comparison of Aroma and Flavor Attributes', 'value': 'aroma-flavor-comparison'}
+            ],
+            value='country-counts',
+            labelStyle={'display': 'block'}
+        ),
+        dcc.Graph(id='graph-output'),
+    ]
 )
+
+@app.callback(
+    Output(component_id='graph-output', component_property='figure'),
+    Input(component_id='show-chart', component_property='value'),
+    Input(component_id='select-graph', component_property='value')
+)
+def update_chart(show_chart, select_graph):
+    graphs = []  # List to hold the selected graphs
+
+    if 'show-country-counts' in show_chart:
+        if select_graph == 'country-counts':
+            return fig_country_counts
+
+    if select_graph == 'aroma-flavor':
+        return fig1
+    elif select_graph == 'aroma-flavor-comparison':
+        return fig2
+
+    return {}
 
 if __name__ == '__main__':
     app.run_server(debug=True)
